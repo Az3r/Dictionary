e3208f197fc765e0d985c1453e712b69c82177c7
Az3r <tuan99kghd@gmail.com>
2020-01-02 23:49:12 +0700

    + Add API Constants

---
+++ Main/APIConstants/LexicalCategories.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.APIConstants
+{
+    public static class LexicalCategories
+    {
+        public const string ADJECTIVE = "adjective";
+        public const string ADVERB = "adverb";
+        public const string COMBININGFORM = "combiningForm";
+        public const string CONJUNCTION = "conjunction";
+        public const string CONTRACTION = "contraction";
+        public const string DETERMINER = "determiner";
+        public const string IDIOMATIC = "idiomatic";
+        public const string INTERJECTION = "interjection";
+        public const string NOUN = "noun";
+        public const string NUMERAL = "numeral";
+        public const string OTHER = "other";
+        public const string PARTICLE = "particle";
+        public const string PREDETERMINER = "predeterminer";
+        public const string PREFIX = "prefix";
+        public const string PREPOSITION = "preposition";
+        public const string PRONOUN = "pronoun";
+        public const string RESIDUAL = "residual";
+        public const string SUFFIX = "suffix";
+        public const string VERB = "verb";
+    }
+    public static class Filters
+    {
+        public const string REGISTERS = "registers";
+        public const string INFLECTIONS = "inflections";
+        public const string TRANSLATIONS = "translations";
+        public const string LEXICAL_CATEGORY = "lexicalCategory";
+        public const string ANTONYMS = "antonyms";
+        public const string DEFINITIONS = "definitions";
+        public const string PRONUNCIATIONS = "pronunciations";
+        public const string GRAMMATICAL_FEATURES = "grammaticalFeatures";
+        public const string REGIONS = "regions";
+        public const string EXAMPLES = "examples";
+        public const string DOMAINS = "domains";
+        public const string SYNONYMS = "synonyms";
+    }
+}
---
+++ Main/Models/LexcicalEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.Models
+{
+    /// <summary>
+    /// Use to contain information of a given word <br></br>
+    /// Information: definitions, pronunciations
+    /// </summary>
+    public class LexcicalEntry
+    {
+
+    }
+}
