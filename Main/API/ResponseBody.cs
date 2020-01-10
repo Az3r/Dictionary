2c4a017bd1a3a508cf3574d0f8321c5d97c9ca99
Az3r <tuan99kghd@gmail.com>
2020-01-10 21:36:08 +0700

    Update ResponseBody.cs

--- Main/API/ResponseBody.cs
+++ Main/API/ResponseBody.cs
@@ -11,7 +11,7 @@ namespace Main.API
     public class ResponseBody
     {
         [JsonProperty(JSONProperties.METADATA)]
-        public ResponseMetadata Metadata { get; set; }
+        public Metadata Metadata { get; set; }
 
         [JsonProperty(JSONProperties.RESULTS)]
         public List<ResponseResult> Results { get; set; }
